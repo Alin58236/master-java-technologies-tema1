ba33883f708e54aa10e53fa33963a823cf770412
Alin Stoia <alin58236@gmail.com>
2025-10-01 21:32:46 +0300

    Refactor and update java version

--- TomcatServletApp/src/main/java/com/sa/tpjad/tomcat/HelloServlet.java
+++ TomcatServletApp/src/main/java/com/sa/tpjad/tomcat/HelloServlet.java
@@ -9,6 +9,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 import static jdk.nashorn.internal.runtime.regexp.joni.Config.log;
@@ -17,30 +18,34 @@ import static jdk.nashorn.internal.runtime.regexp.joni.Config.log;
 public class HelloServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         System.out.println("/GET HelloServlet");
 
         String datetime = "Habar n-am";
 
         try{
-
-            //request catre JettyServletApp
-            URL url = new URL("http://localhost:8081/JettyServletApp/datetime");
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-            con.setRequestMethod("GET");
-
-            //Formatare raspuns
-            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-            datetime = in.readLine();
-            in.close();
-
-            System.out.println("Received from JettyServletApp: "+datetime);
+            datetime = callJetty("http://localhost:8081/JettyServletApp/datetime");
         }catch (Exception e){
-            System.err.println("ERROR connecting to JettyServletApp: "+e.getMessage());
-            System.out.println("Using default datetime.");
+            System.err.println("ERROR connecting to JettyServletApp: "+e.getMessage()+"\nUsing default datetime.\"");
         }
 
         resp.setContentType("text/plain");
         resp.getWriter().println("Servus! Ora este: "+datetime);
+    }
+
+    public String callJetty(String urlString) throws IOException {
+
+        //request catre JettyServletApp
+        URL url = new URL(urlString);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+
+        //Formatare raspuns
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String datetime = in.readLine();
+        System.out.println("Received from JettyServletApp: "+datetime);
+        in.close();
+
+        return datetime;
     }
 }
