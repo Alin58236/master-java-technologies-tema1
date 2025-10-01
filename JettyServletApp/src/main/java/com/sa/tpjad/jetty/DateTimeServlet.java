95256cea550cc55e42104a86f771a00a082754d0
Alin Stoia <alin58236@gmail.com>
2025-10-01 14:10:38 +0300

    Added Datetime Service for Jetty Deployment

---
+++ JettyServletApp/src/main/java/com/sa/tpjad/jetty/DateTimeServlet.java
@@ -0,0 +1,29 @@
+package com.sa.tpjad.jetty;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+
+@WebServlet("/datetime")
+public class DateTimeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        System.out.println("/GET DateTimeServlet/datetime");
+
+        resp.setContentType("text/plain");
+
+        String now = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")).toString();
+
+        System.out.println("Ora returnata: "+now);
+
+        resp.getWriter().write(now);
+    }
+}
